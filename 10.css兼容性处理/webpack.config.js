e59f470a0829d6ad4b3180154babc57474c2136a
814657780 <18037818812@163.com>
2020-12-30 22:29:22 +0800

    css兼容性处理

--- 10.css兼容性处理/webpack.config.js
+++ 10.css兼容性处理/webpack.config.js
@@ -1,51 +1,89 @@
-const {resolve} = require('path');
+const {
+    resolve
+} = require('path');
 // 处理index.html
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 // 从js中抽取css，抽到css文件中
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 // 清除打包构建后的文件夹build
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
 // webpack是基于node环境，所以遵循commonjs规范
 module.exports = {
     // 入口文件
-    entry:'./src/js/index.js',
+    entry: './src/js/index.js',
     // 出口文件
-    output:{
+    output: {
         // 文件名
-        filename:'build/js/built.js',
-        path:resolve(__dirname,'build')
+        filename: 'build/js/built.js',
+        path: resolve(__dirname, 'build')
 
     },
     // loader 配置
-    module:{
-       rules:[
-           {
-             test:/\.css$/,
-             use:[
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [
                 //  把样式从js抽取出来，用style标签加载到html文件中
                 //  'style-loader'
                 // MiniCssExtractPlugin.loader用于把css从js中抽离出来，抽取到css文件里面，所以就用不到style-loader了。
                 MiniCssExtractPlugin.loader,
-                'css-loader'
-             ]
-           }
-       ]
+                'css-loader',
+                /*
+                    css兼容性处理：postcss--->postcss-loader postcss-preset-env
+                    postcss-preset-env帮postcss找到package.json中browserslist里面的配置，通过配置加载指定的css兼容性样式
+                    
+                    "browserslist":{
+     "development":[
+        开发环境
+    //   last 1 chrome version意思是兼容最近的一个chrome浏览器版本
+       "last 1 chrome version",
+       "last 1 firefox version",
+       "last 1 safari version"
+     ],
+     生产环境：默认是看生产环境
+     "production":[
+       ">0.2%",// 大于99.8%的浏览器
+       "not dead",
+       "not op_mini all"
+     ]
+  },
+                    */
+                //  使用loader有两种方式，1、使用loader的默认配置。2、修改loader的配置  
+                //  使用loader的默认配置，如下
+                //  ’postcss-loader‘  
+                //  修改loader的配置
+                {
+                    loader: 'postcss-loader',
+                    options: {
+                        ident: 'postcss-loader',
+                        plugins: () => {
+                            // postcss的插件
+                            require('postcss-preset-env')()
+
+                        }
+
+                    }
+                }
+            ]
+        }]
     },
     // plugin的配置
-    plugins:[
+    plugins: [
         new HtmlWebpackPlugin({
-            template:'./src/index.html'
+            template: './src/index.html'
         }),
         new CleanWebpackPlugin({
-            cleanAfterEveryBuildPatterns:['build']
+            cleanAfterEveryBuildPatterns: ['build']
         }),
         // 提取js文件的css文件
         new MiniCssExtractPlugin({
             // 对输出的文件进行重命名
-            filename:'build/css/built.css'
+            filename: 'build/css/built.css'
         })
 
     ],
     // 模式
-    mode:'development'
+    mode: 'development'
 }
