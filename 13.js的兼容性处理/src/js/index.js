64ced0bed2169a8b06b72f835dd7bd648f894e34
814657780 <18037818812@163.com>
2021-01-09 23:35:35 +0800

    “webpack”

--- 13.js的兼容性处理/src/js/index.js
+++ 13.js的兼容性处理/src/js/index.js
@@ -1,4 +1,13 @@
+import '@babel/polyfill';
 const add = (x,y)=>{
     return x+y;
 }
 console.log(add(1,2))
+
+// promise接受一个函数作为参数，这个函数又接受两个参数，分别是resolve和reject这两个函数
+const promise = new Promise((resolve)=>{
+   setTimeout(()=>{
+       resolve(10)
+   },1000)
+})
+console.log(promise)
--- 13.js的兼容性处理/webpack.config.js
+++ 13.js的兼容性处理/webpack.config.js
@@ -22,7 +22,12 @@ module.exports = {
                 ]
             },
             /*
-              js的兼容性处理-- babel-loader @babel/preset-env   需要安装这两个
+              js的兼容性处理-- babel-loader @babel/core @babel/preset-env,然后下面是三种解决兼容性的方案，分别下载不同的包
+              1、基本的js兼容性处理 ---> @babel/preset-env
+                 问题：只能转换基本语法，如prmise不能转换
+              2、全部js兼容性处理 --> @babel/polyfill
+                 问题：我只要解决部分兼容性问题，但是将所有兼容性代码全部引入，体积太大了~
+              3、需要做兼容性的就做：按需加载。 --> core-js
             */ 
             {
                 test:/\.js$/,
@@ -30,7 +35,29 @@ module.exports = {
                 loader:'babel-loader',
                 options:{
                     // 预设：指示babel做怎样的兼容性处理。
-                    presets:['@babel/preset-env']
+                    presets:[
+                        [
+                            '@babel/preset-env',
+                            {
+                                // 按需加载
+                                useBuiltIns:'usage',
+                                // 指定core-js版本
+                                corejs:{
+                                    version:3
+                                },
+                                // 指定兼容性做到哪个版本浏览器
+                                targets:{
+                                    chrome:'60',
+                                    firefox:'60',
+                                    ie:'9',
+                                    safari:'10',
+                                    edge:'17'
+
+                                }
+                            }
+
+                        ]
+                    ]
                 }
             }
         ]
