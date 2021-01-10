7aee82b775d644da62cdfa76a632b478eb280933
814657780 <18037818812@163.com>
2021-01-10 17:56:04 +0800

    “webpack”

---
+++ 14.js压缩/webpack.config.js
@@ -0,0 +1,30 @@
+const{resolve} = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+// 因为webpack基于node，所以遵循commonjs规范
+module.exports = {
+    // 入口文件
+    entry:'./src/js/index.js',
+    // 出口文件
+    output:{
+        filename:'js/built.js',
+        path:resolve(__dirname,'build')
+    },
+    // loader的配置
+    module:{
+        rules:[
+
+        ]
+    },
+    // plugins插件的配置
+    plugin:[
+        new HtmlWebpackPlugin({
+            template:'./src/index.html'
+        }),
+        new CleanWebpackPlugin({
+            cleanAfterEveryBuildPatterns:['build']
+        })
+    ],
+    // 模式
+    mode:'development'
+}
