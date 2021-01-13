2a30c222f80f588e6addbe481f2e41ec2c8512a1
814657780 <18037818812@163.com>
2021-01-13 22:15:27 +0800

    “webpack”

--- 18.HMR/webpack.config.js
+++ 18.HMR/webpack.config.js
@@ -2,6 +2,10 @@
   HMR: hot module replacement 热模块替换/模块热替换
   作用：一个模块发生变化，只会重新打包这一个模块（而不是打包所有模块）
   极大的提高了构建速度
+
+  样式文件：可以使用HMR功能：因为style-loader内部实现了~
+  js文件：默认不能使用HMR功能
+  html文件：默认不能使用HMR功能，同时会导致问题：html文件不能热更新了~
 */ 
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
