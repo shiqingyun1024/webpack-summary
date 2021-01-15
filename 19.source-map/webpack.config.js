c0eec2f8a786167bcc18cafec19fe0dd41aa7bf7
814657780 <18037818812@163.com>
2021-01-15 22:50:12 +0800

    “webpack”

--- 19.source-map/webpack.config.js
+++ 19.source-map/webpack.config.js
@@ -1,3 +1,47 @@
 /*
 source-map:一种提供源代码到构建后代码的映射技术（如果构建后代码出错了，通过映射可以追踪源代码错误）非常利于调试，去找错误的原因
 */ 
+const {resolve} = require('path');
+// webpack是基于node配置的，所以遵循commonjs规范
+module.exports = {
+    // 入口文件
+    entry:'./src/js/index.js',
+    // 出口文件
+    output:{
+        filename:'js/built.js',
+        path:resolve(__dirname,'build')
+    },
+    // loader的配置
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:[
+                    // style-loader含有热更新功能，所以css改变之后会自动热更新。
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                
+            }
+
+        ]
+    },
+    // plugin插件的配置
+    plugins:[
+        new HtmlWebpackPlugin({
+            template:'./src/index.html'
+        }),
+        new CleanWebpackPlugin({
+            cleanAfterEveryBuildPatterns:['build']
+        })
+
+    ],
+    // 模式
+    mode:'development',
+    // 开发服务器
+    devServer:{
+
+    }
+}
