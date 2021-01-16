3bc47b1bfde6a91fe6ee55aea5396ccee7f29178
814657780 <18037818812@163.com>
2021-01-16 22:22:45 +0800

    “webpack”

--- 19.source-map/webpack.config.js
+++ 19.source-map/webpack.config.js
@@ -1,25 +1,25 @@
 /*
 source-map:一种提供源代码到构建后代码的映射技术（如果构建后代码出错了，通过映射可以追踪源代码错误）非常利于调试，去找错误的原因
-*/ 
+*/
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('html-webpack-plugin');
-const {resolve} = require('path');
+const { CleanWebpackPlugin } = require('html-webpack-plugin');
+const { resolve } = require('path');
 // webpack是基于node配置的，所以遵循commonjs规范
 module.exports = {
     // 入口文件
-    entry:'./src/js/index.js',
+    entry: './src/js/index.js',
     // 出口文件
-    output:{
-        filename:'js/built.js',
-        path:resolve(__dirname,'build')
+    output: {
+        filename: 'js/built.js',
+        path: resolve(__dirname, 'build')
     },
     // loader的配置
-    module:{
-        rules:[
+    module: {
+        rules: [
             {
                 // 处理css文件
-                test:/\.css$/,
-                use:[
+                test: /\.css$/,
+                use: [
                     // style-loader含有热更新功能，所以css改变之后会自动热更新。
                     'style-loader',
                     'css-loader'
@@ -27,35 +27,44 @@ module.exports = {
             },
             {
                 // 处理图片资源
-                test:/\.(png|jpeg|jpg|gif)$/,
-                loader:'url-loader'
+                test: /\.(png|jpeg|jpg|gif)$/,
+                loader: 'url-loader'
             },
             {
                 // 处理html中的图片
-                test:/\.html$/,
-                loader:'html-loader'
+                test: /\.html$/,
+                loader: 'html-loader'
             },
             {
                 // 处理其他资源
-                exculde:/\.(html|css|png|jpeg|jpg|gif)/
+                exculde: /\.(html|css|png|jpeg|jpg|gif)/
             }
 
         ]
     },
     // plugin插件的配置
-    plugins:[
+    plugins: [
         new HtmlWebpackPlugin({
-            template:'./src/index.html'
+            template: './src/index.html'
         }),
         new CleanWebpackPlugin({
-            cleanAfterEveryBuildPatterns:['build']
+            cleanAfterEveryBuildPatterns: ['build']
         })
 
     ],
     // 模式
-    mode:'development',
+    mode: 'development',
     // 开发服务器
-    devServer:{
-
+    devServer: {
+        // 项目构建后路径
+        contentBase: resolve(__dirname, 'build'),
+        //  启动gzip压缩
+        compress: true,
+        // 启动服务的端口号
+        port: 3000,
+        // 自动打开浏览器
+        open: true,
+        // 开启HMR功能
+        hot: true
     }
 }
