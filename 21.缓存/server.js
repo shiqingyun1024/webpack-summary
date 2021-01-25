8ad729ca2166f9a2df627cc9055f68443cb31e04
814657780 <18037818812@163.com>
2021-01-25 21:59:20 +0800

    “webpack”

--- 21.缓存/server.js
+++ 21.缓存/server.js
@@ -1,7 +1,13 @@
 /*
   服务器代码
   启动服务器指令：
+  先安装nodemon   npm i nodemon -g
   nodemon server.js
+  或者
+  不安装包，直接运行 node server.js
+
+  访问服务器地址：
+  http://localhost:8080
 */ 
 const express = require('express');
 
