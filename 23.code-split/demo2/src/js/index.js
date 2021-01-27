7747127e74de9f1fd309ced85a24753fce2a5943
814657780 <18037818812@163.com>
2021-01-27 22:59:24 +0800

    “webpack”

--- 23.code-split/demo2/src/js/index.js
+++ 23.code-split/demo2/src/js/index.js
@@ -1,6 +1,8 @@
+import $ from 'jquery'
 // import mul from './test'
 function add(x,y){
     return x+y;
 }
 console.log(add(1,8))
 // console.log(mul(1,8))
+console.log($)
--- 23.code-split/demo2/webpack.config.js
+++ 23.code-split/demo2/webpack.config.js
@@ -5,7 +5,13 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 module.exports = {
     // 入口文件
     // 单入口  单页面应用程序会这样写
-    entry:'./src/js/index.js',
+    // entry:'./src/js/index.js',
+    entry:{
+        // 多入口: 每个入口输出一个bundle
+      //   main:'./src/js/index.js',
+        index:'./src/js/index.js',
+        test:'./src/js/test.js'
+      },
     // 出口文件
     output:{
         // [name]:取文件名，对应entry中的main和test，如果把main改为index，那么输出的文件名就是index.hash.js
@@ -34,6 +40,10 @@ module.exports = {
 
     ],
     // 代码分割配置
+    /*
+      1、单页面应用 可以将node_modules中代码单独打包成一个chunk最终输出，打包的时候自己写的单独打成一个包，第三方文件单独打成一个包
+      2、多页面应用 自动分析多入口文件中，有没有公共的文件。如果有，会打包成单独的一个文件
+    */ 
     optimization:{
         splitChunks:{
             chunks:'all'
