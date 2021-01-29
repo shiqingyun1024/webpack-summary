eecad71537192a649ee75b628ac3d5e05fd08280
814657780 <18037818812@163.com>
2021-01-29 23:52:32 +0800

    “webpack”

--- 23.code-split/demo3/src/js/index.js
+++ 23.code-split/demo3/src/js/index.js
@@ -10,10 +10,10 @@ console.log($)
   通过js代码，让某个文件单独打包成一个chunk
 */ 
 import('./test')
-   .then((result)=>{
+   .then(({mul})=>{
       //  文件加载成功  
        console.log('1234')
-       console.log(result)
+       console.log(mul)
    })
    .catch(()=>{
        console.log('文件加载失败~');
