b8460b9ed6eaf0ffc13e6a51fc2a68cf3902ddba
814657780 <18037818812@163.com>
2021-02-01 23:22:35 +0800

    “webpack”

--- 25.PWA/src/js/index.js
+++ 25.PWA/src/js/index.js
@@ -1,28 +1,40 @@
-import {mul} from './test';
-import "../index.less"
+import 'core-js/modules/es.array.reduce.js';
+
+import { mul } from './test';
+import '../index.less';
+
 function add(x, y) {
-    return x + y;
+  return x + y;
 }
+
 console.log(add(1, 2));
 
-function sum(...args){
-    return args.reduce((p,c)=>p+c,0)
+function sum() {
+  for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+    args[_key] = arguments[_key];
+  }
+
+  return args.reduce((p, c) => p + c, 0);
 }
 
-console.log(sum(1,2,3,4))
-
-console.log(mul(2,8));
-
+console.log(sum(1, 2, 3, 4));
+console.log(mul(2, 8));
+/*
+  1. eslint不认识 window、navigator全局变量
+    解决：需要修改package.json中eslintConfig配置
+    "env":{
+      "browser":true // 支持浏览器端全局变量,如果是node端，就写"node":true
+    }
+*/
 // 注册serviceworker
 // 处理兼容性问题
-if('serviceworker' in navigator){
-    window.addEventListener('load',()=>{
-        navigator.serviceWorker.register('/service-worker.js')
-        .then(()=>{
-            console.log('servicework注册成功了~')
-        })
-        .catch(()=>{
-            console.log('servicework注册失败了~')
-        })
-    })
+
+if ('serviceworker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(() => {
+      console.log('servicework注册成功了~');
+    }).catch(() => {
+      console.log('servicework注册失败了~');
+    });
+  });
 }
--- 25.PWA/src/js/test.js
+++ 25.PWA/src/js/test.js
@@ -1,6 +1,6 @@
-export function mul(x,y){
-    return x*y;
+export function mul(x, y) {
+  return x * y;
 }
-export function count(x,y){
-    return x-y
+export function count(x, y) {
+  return x - y;
 }
