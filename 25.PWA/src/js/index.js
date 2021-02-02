d70ea980126921510493aad90765877d27d9cabf
814657780 <18037818812@163.com>
2021-02-02 21:58:07 +0800

    “webpack”

--- 25.PWA/src/js/index.js
+++ 25.PWA/src/js/index.js
@@ -26,12 +26,16 @@ console.log(mul(2, 8));
       "browser":true // 支持浏览器端全局变量,如果是node端，就写"node":true
     }
 
-  2. serviceworker代码必须运行在服务器上 
+  2. serviceworker代码必须运行在服务器上
+  ---> nodejs
+  --->
+     npm i serve -g
+     serve -s build 启动服务器，将build目录下所有资源作为静态资源暴露出去
 */
 // 注册serviceworker
 // 处理兼容性问题
 
-if ('serviceworker' in navigator) {
+if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/service-worker.js').then(() => {
       console.log('servicework注册成功了~');
