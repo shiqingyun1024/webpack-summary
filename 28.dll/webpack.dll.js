dad752ecbcca1be7171826eb8cb30b755f1add54
814657780 <18037818812@163.com>
2021-02-04 22:29:45 +0800

    “webpack”

--- 28.dll/webpack.dll.js
+++ 28.dll/webpack.dll.js
@@ -1,8 +1,11 @@
 /*
 使用dll技术，对某些库（第三方库：jquery、react、vue...）进行单独打包
+当你运行webpack时，默认查找webpack.config.js 配置文件
+需求：需要运行 webpack.dll.js 文件
+   ---> webpack --config webpack.dll.js
 */ 
 const {resolve} = require('path');
-const webpack = require('webapck');
+const webpack = require('webpack');
 module.exports = {
     entry:{
         // 最终打包生成的[name]---->jquery
@@ -20,5 +23,7 @@ module.exports = {
             name:'[name]_[hash:8]',//映射库的暴露的内容名称
             path:resolve(__dirname,'dll/manifest.json'), // 输出文件路径
         })
-    ]
+    ],
+    // 模式
+    mode:'production'
 }
