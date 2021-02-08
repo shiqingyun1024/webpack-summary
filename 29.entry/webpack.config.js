9f3d346ec8cdf34474312e76a57a96735dfc36da
814657780 <18037818812@163.com>
2021-02-08 21:41:32 +0800

    “webpack”

--- 29.entry/webpack.config.js
+++ 29.entry/webpack.config.js
@@ -7,16 +7,31 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 /*
   entry: 入口起点
    1. string ---> './src/index.js'
-      打包形成一个chunk。 输出一个bundle
-   2. array
+      单入口
+      打包形成一个chunk。 输出一个bundle文件。
+      此时chunk的名称默认是main
+   2. array ---> ['./src/index.js','./src/add.js']
+      多入口
+      所有入口文件最终只会形成一个chunk，输出出去只有一个bundle文件。
+      -->只有在HMR功能中让html热更新生效~
    3. object
+      多入口
+      有几个入口文件就形成几个chunk，输出几个bundle文件
+      此时chunk的名称是key
+      
+
 */ 
 module.exports={
     // 入口文件
-    entry:'./src/index.js',
+    // entry:'./src/index.js',
+    // entry:['./src/index.js','./src/add.js'],
+    entry:{
+        index:'./src/index.js',
+        count:'./src/count.js'
+    },
     // 出口文件
     output:{
-        filename:'built.js',
+        filename:'[name].[contenthash:8].js',
         path:resolve(__dirname,'build')
     },
     // loader
