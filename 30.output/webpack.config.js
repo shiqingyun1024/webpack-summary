c6f64e98985bc10519662402da7a97fa98448534
814657780 <18037818812@163.com>
2021-02-09 23:04:52 +0800

    “webpack”

--- 30.output/webpack.config.js
+++ 30.output/webpack.config.js
@@ -4,43 +4,17 @@
 */ 
 const {resolve} = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-/*
-  entry: 入口起点
-   1. string ---> './src/index.js'
-      单入口
-      打包形成一个chunk。 输出一个bundle文件。
-      此时chunk的名称默认是main
-   2. array ---> ['./src/index.js','./src/add.js']
-      多入口
-      所有入口文件最终只会形成一个chunk，输出出去只有一个bundle文件。
-      -->只有在HMR功能中让html热更新生效~
-   3. object
-      多入口
-      有几个入口文件就形成几个chunk，输出几个bundle文件
-      此时chunk的名称是key
-
-
-   ---> 特殊用法
-   {
-       // 所有入口文件最终只会形成一个chunk，输出出去只有一个bundle文件。
-       index:['./src/index.html','./src/count.js'],
-       // 形成一个chunk，输出一个bundle文件
-       add:'./src/add.js'
-   }   
-
-*/ 
 module.exports={
     // 入口文件
-    // entry:'./src/index.js',
-    // entry:['./src/index.js','./src/add.js'],
-    entry:{
-        index:'./src/index.js',
-        count:'./src/count.js'
-    },
+    entry:'./src/index.js',
     // 出口文件
     output:{
-        filename:'[name].[contenthash:8].js',
-        path:resolve(__dirname,'build')
+        // 文件名称 （指定名称+目录）
+        filename:'js/[name].[contenthash:8].js',
+        // 输出文件目录（将来所有资源输出的公共目录）
+        path:resolve(__dirname,'build'),
+        // 所有资源引入公共路径前缀 ---> 'imgs/a.jpg' --> '/imgs/a.jpg'
+        // publicPath:'/'
     },
     // loader
     module:{
