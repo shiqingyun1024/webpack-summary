abbcef3e149b42cca092d347b7933b5ce53c3e8b
814657780 <18037818812@163.com>
2021-02-21 16:42:51 +0800

    “webpack”

---
+++ 31.module/build/js/0_chunk.js
@@ -0,0 +1,15 @@
+(window["webpackJsonp_name_"] = window["webpackJsonp_name_"] || []).push([[0],{
+
+/***/ "./src/count.js":
+/*!**********************!*\
+  !*** ./src/count.js ***!
+  \**********************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\nfunction count(x,y){\n   return x - y;\n}\n/* harmony default export */ __webpack_exports__[\"default\"] = (count);\n\n//# sourceURL=webpack://%5Bname%5D/./src/count.js?");
+
+/***/ })
+
+}]);
---
+++ 31.module/webpack.config.js
@@ -0,0 +1,51 @@
+/*
+ loader: 1、下载  2、使用（配置loader）
+ plugins：1、下载 2、引入  3、使用
+*/ 
+const {resolve} = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+module.exports={
+    // 入口文件
+    entry:'./src/index.js',
+    // 出口文件
+    output:{
+        // 文件名称 （指定名称+目录）
+        filename:'js/[name].[contenthash:8].js',
+        // 输出文件目录（将来所有资源输出的公共目录）
+        path:resolve(__dirname,'build'),
+        // 所有资源引入公共路径前缀 ---> 'imgs/a.jpg' --> '/imgs/a.jpg' 一般用于生产环境
+        publicPath:'/',
+        // 非入口chunk的名称
+        chunkFilename:'js/[name]_chunk.js',
+        // library的作用  表示整个库向外暴露的变量名  main.js中的window["main"] =中的"main"
+        library:'[name]',
+        // libraryTarget表示变量名添加到哪个上  main.js中的window["main"] =中的window   这样其他的js文件可以使用main.js中的方法
+        libraryTarget:'window',
+        // libraryTarget:'commonjs' // 这个时候main.js中的开头是 exports["main"]
+    },
+    // loader
+    module:{
+        rules:[
+     //loader的配置 
+        ]
+    },
+    // plugins
+    plugins:[
+        // plugins的配置
+        // html-webpack-plugin
+        // 功能：默认会创建一个空的HTML，自动引入打包输出的所有资源(JS/CSS)
+        // 如果有这样的需求 ，需要有结构的html文件，需要加配置，template
+        new HtmlWebpackPlugin({
+            // 复制./src/index.html文件，并自动引入打包输出的所有资源(JS/CSS)
+            template:'./src/index.html'
+        }),
+        new CleanWebpackPlugin({
+            cleanAfterEveryBuildPatterns: ['build']
+        })
+
+    ],
+    // 模式
+    mode:'development'
+    // mode:'production'
+}
