87b11d34c46d65719c0d735443da9b4978a9ddf2
814657780 <18037818812@163.com>
2021-02-21 17:41:48 +0800

    “webpack”

--- 32.resolve/src/index.js
+++ 32.resolve/src/index.js
@@ -1,6 +1,6 @@
 // import count from './count'
 // import './css/index.css'
-import '$css/index.css'  // 指定了别名路径，可以这样写。
+import '$css/index'  // 指定了别名路径，可以这样写。webpack.config.js中配置了extensions，所以可以不写文件后缀名
 function add(x,y){
    return x+y;
 }
