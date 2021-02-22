c16a4f1b9928e8672ccfb8ea979fe45cd694ff96
814657780 <18037818812@163.com>
2021-02-22 21:43:52 +0800

    “webpack”

--- 34.optimization/webpack.config.js
+++ 34.optimization/webpack.config.js
@@ -74,7 +74,27 @@ module.exports={
             maxSize: 0, // 最大没有限制
             minChunks:1,// 要提取的chunk最少被引用1次
             maxAsyncRequests: 5, // 按需加载时并行加载的文件的最大数量
-            maxInitialRequests: 3, //
+            maxInitialRequests: 3, // 入口js文件最大并行请求数量
+            automaticNameDelimiter:'~', // 名称连接符
+            name:true, // 可以使用命名规则
+            cacheGroups:{  // 分割chunk的组
+                // node_modules文件会被打包到vendors组的chunk中。 --> vendors~xxx.js
+                // 一定要满足上面的公共规则，如：大小超过30kb，至少被引用一次。
+                vendors:{
+                    test:/[\\/]node_modules[\\/]/,
+                    // 优先级
+                    priority: -10
+                },
+                default:{
+                    // 要提取的chunk最少被引用2次
+                    minChunks:2,
+                    // 优先级
+                    priority:-20,
+                    // 如果当前要打包的模块，和之前已经被提取的模块是同一个，就会复用，而不是重新打包模块
+                    reuseExistingChunk:true
+                }
+
+            }
         }
     }
 }
