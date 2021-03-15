7274839edee865437bcefe7cb23157e35570e661
814657780 <18037818812@163.com>
2021-03-15 22:32:47 +0800

    “webpack”

--- 39.loader/webpack.config.js
+++ 39.loader/webpack.config.js
@@ -7,9 +7,20 @@ module.exports = {
         rules:[
             {
                 test: /\.js$/,
-                loader:path.resolve(__dirname,'loaders','loader1')
+                // loader:path.resolve(__dirname,'loaders','loader1')
+                // 因为在resolveLoader中配置了寻找loader的路径，所以可以直接写成下面这样
+                loader:'loader1'
             }
 
         ]
-    }
+    },
+    // resolveLoader 一个解析loader的配置选项   loader的解析规则
+    resolveLoader:{
+        // 定义去哪个模块下找loader
+       modules:[
+           'node_modules',
+           path.resolve(__dirname,'loaders')
+       ]
+    },
+    mode:'development'
 }
