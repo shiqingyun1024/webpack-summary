8addc0c0c4815ae61a42509722899cfac464aa89
814657780 <18037818812@163.com>
2021-03-15 23:03:05 +0800

    “webpack”

--- 39.loader/loaders/loader1.js
+++ 39.loader/loaders/loader1.js
@@ -1,7 +1,7 @@
 // loader 本质上是一个函数
 // content是文件的内容，map是文件的映射信息，meta是文件的元信息
 
-// 同步loader
+// 同步loader  异步loader的写法见loader2.js
 // module.exports = function(content,map,meta){
 //     console.log(111);
 //     return content;
@@ -10,7 +10,7 @@
 // 同步loader 的另外一种写法 使用this.callback来代替return
 module.exports = function(content,map,meta){
     console.log(111);
-    // 有没有错误，没有就传null
+    // 有没有错误，没有就传null，  map,meta这两个参数是可选参数
     this.callback(null,content,map,meta)
 }
 
--- 39.loader/loaders/loader2.js
+++ 39.loader/loaders/loader2.js
@@ -1,10 +1,18 @@
 // loader 本质上是一个函数
 // content是文件的内容，map是文件的映射信息，meta是文件的元信息
+// 同步loader
+// module.exports = function(content,map,meta){
+//     console.log(222);
+//     return content;
+// }
+// 异步loader
 module.exports = function(content,map,meta){
     console.log(222);
-    return content;
-    
-
+    // 异步函数async，当使用callback的时候才会往下执行
+    const callback = this.async();
+    setTimeout(()=>{
+        callback(null,content);
+    },3000);
 }
 
 // pitch方法是按照webpack.config.js中的use顺序从上往下执行的，所以再打印pitch 222
