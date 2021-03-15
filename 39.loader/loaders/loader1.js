1370bf1c73a64efa5fc4288c7e2a20aa5b6fdf66
814657780 <18037818812@163.com>
2021-03-15 22:18:01 +0800

    “webpack”

--- 39.loader/loaders/loader1.js
+++ 39.loader/loaders/loader1.js
@@ -1,4 +1,8 @@
 // loader 本质上是一个函数
-module.exports = function(){
+// content是文件的内容，map是文件的映射信息，meta是文件的元信息
+module.exports = function(content,map,meta){
+    console.log(content);
+    return content;
     
+
 }
--- 39.loader/webpack.config.js
+++ 39.loader/webpack.config.js
@@ -1,11 +1,15 @@
-
+const path = require('path');
 
 
 // webpack5.0中默认使用了entry和output的配置
 module.exports = {
     module:{
         rules:[
-            
+            {
+                test: /\.js$/,
+                loader:path.resolve(__dirname,'loaders','loader1')
+            }
+
         ]
     }
 }
