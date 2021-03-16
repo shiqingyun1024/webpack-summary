979cd395c497ea64485390ac6851c42860d746ec
814657780 <18037818812@163.com>
2021-03-16 22:40:46 +0800

    “webpack”

--- 39.loader/loaders/babelLoader.js
+++ 39.loader/loaders/babelLoader.js
@@ -2,7 +2,17 @@
 // loader 本质上是一个函数
 // content是文件的内容，map是文件的映射信息，meta是文件的元信息
 // module.exports是commonjs规范
-module.exports = function(content,map,meta){
+const { getOptions } = require('loader-utils')
+// 校验options是否满足校验规则
+const { validate } = require('schema-utils')
 
+const babelSchema = require('./babelSchema.json');
+module.exports = function (content, map, meta) {
+    //  获取loader的options配置
+    const options = getOptions(this);
+    // 校验babel的options的配置
+    validate(babelSchema, options, {
+        name: 'Babel Loader'
+    })
 
 }
