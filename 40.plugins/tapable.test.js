12f9dbc0f1cfa3fb04ca413efc51f7f26035a622
814657780 <18037818812@163.com>
2021-03-17 22:36:38 +0800

    “webpack”

--- 40.plugins/tapable.test.js
+++ 40.plugins/tapable.test.js
@@ -1,1 +1,13 @@
-const {} = require('tapable');
+const { SyncHook, SyncBailHook, AsyncParallelHook, AsyncSeriesHook } = require('tapable');
+class Lesson{
+    constructor(){
+        // 初始化hooks容器
+        this.hooks = {
+            go:new SyncHook(['address'])
+        }
+    }
+}
+
+
+
+
