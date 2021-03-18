6a5ffb5fc8108a8efb436e59a42e3ad6ebae1bbc
814657780 <18037818812@163.com>
2021-03-18 22:42:30 +0800

    “webpack”

--- 40.plugins/tapable.test.js
+++ 40.plugins/tapable.test.js
@@ -7,8 +7,10 @@ class Lesson{
             // go:new SyncHook(['address'])  数组中可以传n个参数
             go:new SyncBailHook(['address']), // 一旦遇到return就不往下执行了
 
-            // AsyncParallelHook, AsyncSeriesHook 都是异步的钩子
-            leave:new AsyncParallelHook(['name','age'])
+            // AsyncParallelHook 是异步的钩子,  异步并行执行
+            // leave:new AsyncParallelHook(['name','age']),
+            // AsyncSeriesHook ：异步串行（按顺序执行）
+            leave:new AsyncSeriesHook(['name','age'])
         }
     }
     tap(){
@@ -20,15 +22,32 @@ class Lesson{
         this.hooks.go.tap('class0340',(address)=>{
             console.log('class0340',address);
         })
-        this.hooks.leave.tap('class0510',(address)=>{
-            console.log('class0510',address);
+        // cb就是callback
+        this.hooks.leave.tapAsync('class0510',(name,age,cb)=>{
+            setTimeout(()=>{
+                console.log('class0510',name,age);
+                // 一定要调用cb()
+                cb();
+            },2000)
+        })
+        // 第二种异步的写法
+        this.hooks.leave.tapPromise('class0630',(name,age)=>{
+            return new Promise((resolve)=>{
+                setTimeout(()=>{
+                    console.log('class0630',name,age);
+                    resolve();
+                },1000)
+            })
         })
     }
 
     start(){
         // 触发hooks
         this.hooks.go.call('c318');
-        this.hooks.leave.call('jack',18);
+        this.hooks.leave.callAsync('jack',18,function(){
+            // 代表所有leave容器中的函数触发完了，才触发
+            console.log('end~~~');
+        });
     }
 }
 
@@ -41,6 +60,19 @@ l.start();
 
 // SyncBailHook打印结果如下：
 // class0318 c318
+
+// AsyncParallelHook打印结果如下（并行执行，先输出class0630再输出class0510）：
+// class0318 c318
+// class0630 jack 18
+// class0510 jack 18
+
+// AsyncSeriesHook打印结果如下（顺序执行）：
+// class0318 c318
+// class0510 jack 18
+// class0630 jack 18
+
+
+
 
 
 
