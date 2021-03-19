e300b8ad12c2d6d4e7b44d988576a6102a8ba181
814657780 <18037818812@163.com>
2021-03-19 23:09:05 +0800

    “webpack”

--- 40.plugins/plugins/Plugin1.js
+++ 40.plugins/plugins/Plugin1.js
@@ -1,6 +1,9 @@
 // 每一个plugins都是一个类或者构造函数，都是new出来的
 
 class Plugin1{
+    constructor(){
+        
+    }
     // 每个plugin都有一个apply方法，new的时候调用
     apply(complier){
         // 使用complier上的一些钩子去做一些操作,可以绑定多个
