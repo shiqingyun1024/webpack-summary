5da96b2c434e9087458a70ad382e063295bf5af8
814657780 <18037818812@163.com>
2021-03-19 21:43:00 +0800

    “webpack”

---
+++ 40.plugins/plugins/Plugin1.js
@@ -0,0 +1,18 @@
+// 每一个plugins都是一个类或者构造函数，都是new出来的
+
+class Plugin1{
+    // 每个plugin都有一个apply方法，new的时候调用
+    apply(){
+
+    }
+
+}
+// module.export = Plugin1;
+export default Plugin1;
+
+
+
+
+
+
+
--- 40.plugins/webpack.config.js
+++ 40.plugins/webpack.config.js
@@ -1,5 +1,8 @@
+const Plugin1 = require('./plugins/Plugin1');
+
 module.exports = {
     plugins:[
+        new Plugin1()
         
     ]
 }
