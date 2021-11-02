293717a9db2139519918cfa72b1f94c879625024
814657780 <18037818812@163.com>
2021-11-02 20:59:55 +0800

    Vue打包优化-加快首屏加载速度

---
+++ 41.Vue打包优化-加快首屏加载速度/cdn/src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueRouter from 'vue-router'
+import Vuex from 'vuex'
+import axios from 'axios'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(VueRouter);
+Vue.use(Vuex);
+Vue.use(ElementUI);
+
+const instance = axios.create()
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
